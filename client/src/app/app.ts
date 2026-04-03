23fa207c87d2bc6ca42886374d67ebe320264902
jstnroxx <ms325358@gmail.com>
2026-04-03 18:22:14 +0500

    refactor: rename libroshare-client to client

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('libroshare-client');
+  protected readonly title = signal('client');
 }
