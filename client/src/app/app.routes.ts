76729a2064f609e1c2cc69058de712cb7a59932e
Ayau <shaymerdena@gmail.com>
2026-04-11 22:41:48 +0500

    feat: generate initial components and setup routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { BookList } from './components/book-list/book-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'books', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'books', component: BookList }
+];
