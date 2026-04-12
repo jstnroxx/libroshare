9ca04853fa70316dced26111e306046cd5380c16
Ayau <shaymerdena@gmail.com>
2026-04-12 17:07:20 +0500

    feat: complete navbar styling

--- client/src/app/components/navbar/navbar.ts
+++ client/src/app/components/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css'],
 })
 export class Navbar {
 
