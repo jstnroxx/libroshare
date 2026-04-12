4d2c14e7d147942774d24e981d6c8edf53b4691e
cipher <shaymerdena@gmail.com>
2026-04-12 17:43:30 +0500

    Setup project architecture, navigation, and global styles (#1)
    * chore: install dependencies and verify environment
    
    * feat: generate initial components and setup routing
    
    * feat: complete navbar styling
    
    * feat: add navbar and router-outlet to app component
    
    * style: add global fonts and reset default browser styles
    
    * feat: setup home component
    
    * feat: setup home-based routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
+import { BookList } from './components/book-list/book-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'login', component: Login },
+    { path: 'books', component: BookList }
+];
---
+++ client/src/app/components/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css'],
+})
+export class Navbar {
+
+}
