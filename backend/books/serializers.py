a99def917a45dc366b9904f7c2db6a0b2e4a66f3
Aya <ayana.takoch@gmail.com>
2026-04-20 09:52:40 +0500

    feat: base backend API with auth, books and requests

--- backend/books/serializers.py
+++ backend/books/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from .models import Book, Request
+from django.contrib.auth.models import User
+from .models import Book, BookOffer, Review, Request, Profile
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = '__all__'
+
+
+class BookOfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookOffer
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
 
 
@@ -14,11 +27,16 @@ class RequestSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
 
 
-class ReviewSerializer(serializers.Serializer):
-    rating = serializers.IntegerField()
-    comment = serializers.CharField()
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
