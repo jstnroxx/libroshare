6774c85c1344716918590aef0b6fe7a22c27e9f2
jstnroxx <ms325358@gmail.com>
2026-04-21 21:30:29 +0500

    feat: create login page

--- client/src/app/components/login/login.ts
+++ client/src/app/components/login/login.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginMode: boolean = true
 
+  username: string = '';
+  password: string = '';
+  confirmedPassword: string = '';
+  isSubmitting: boolean = false;
+
+  private authService = inject(Auth)
+  private router = inject(Router)
+
+  clearInputs(): void {
+    this.username = '';
+    this.password = '';
+    this.confirmedPassword = '';
+    this.isSubmitting = false
+  }
+
+  login(): void {
+    this.isSubmitting = true
+
+    setTimeout(() => {
+      this.clearInputs()
+      this.authService.logIn()
+      this.router.navigate(['/books'])
+    }, 1000)
+  }
+
+  register(): void {
+    this.isSubmitting = true
+
+    setTimeout(() => {
+      this.clearInputs()
+      this.authService.logIn()
+      this.router.navigate(['/books'])
+    }, 1000)
+  }
+
+  toggleMode(): void {
+    this.loginMode = !this.loginMode
+  }
 }
