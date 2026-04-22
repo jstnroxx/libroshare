98d50f746df2bb31a14a65e5380a4d15702265ab
Aya <ayana.takoch@gmail.com>
2026-04-22 16:16:27 +0500

    feat: add my-shelf

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('reviews/', views.ReviewAPIView.as_view()),
     path('stats/', views.book_stats),
     path('dashboard/', views.user_dashboard),
+    
+    path('my-shelf/', views.MyShelfView.as_view(), name='my-shelf'),
+    path('add-instance/', views.AddInstanceView.as_view(), name='add-instance'),
 ]
